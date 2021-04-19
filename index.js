8ff91879a10df4998307f8a623dc0bbcd2d5bd82
Peter Godspower <iamgpower3@gmail.com>
2021-04-19 23:54:25 +0100

    Renamed the folder path
    Renamed folder path from ./results to ./result

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ http.get('http://jsonplaceholder.typicode.com/posts', (resp) => {
 	});
 
 	resp.on('end', () => {
-		if (fs.existsSync('./results/post.json')) {
+		if (fs.existsSync('./result/post.json')) {
 			console.log('/results/post.json already exist');
 			return true;
 		}
 
-		fs.mkdir('./results', { recursive: true }, (err) => {
+		fs.mkdir('./result', { recursive: true }, (err) => {
 			if (err) {
 				throw err;
 			}
 			console.log('Folder successfully created');
 		});
 
-		fs.writeFile('./results/post.json', data, (err) => {
+		fs.writeFile('./result/post.json', data, (err) => {
 			if (err) {
 				throw err;
 			}

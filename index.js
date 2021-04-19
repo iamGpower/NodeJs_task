64585748f3dc13ba5c4519b3983d352348f741d2
Peter Godspower <iamgpower3@gmail.com>
2021-04-19 23:15:01 +0100

    code refactored

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ http.get('http://jsonplaceholder.typicode.com/posts', (resp) => {
 	});
 
 	resp.on('end', () => {
-		if (fs.existsSync(`${__dirname}/results/post.json`)) {
+		if (fs.existsSync('./results/post.json')) {
 			console.log('/results/post.json already exist');
 			return true;
 		}
 
-		fs.mkdir(`${__dirname}/results`, { recursive: true }, (err) => {
+		fs.mkdir('./results', { recursive: true }, (err) => {
 			if (err) {
 				throw err;
 			}
 			console.log('Folder successfully created');
 		});
 
-		fs.writeFile(`${__dirname}/results/post.json`, data, (err) => {
+		fs.writeFile('./results/post.json', data, (err) => {
 			if (err) {
 				throw err;
 			}
